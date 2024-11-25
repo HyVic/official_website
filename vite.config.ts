9f14c5d7c29775af93ce45981f166d87accdd4d8
729203471@qq.com <729203471@qq.com>
2024-11-25 15:08:50 +0800

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 压缩gzip
 import viteCompression from 'vite-plugin-compression'
-// import { visualizer } from 'rollup-plugin-visualizer'
-// import viteImagemin from "vite-plugin-imagemin"
+// 打包进度
+import progress from 'vite-plugin-progress'
+// 自动重启
+import ViteRestart from 'vite-plugin-restart'
+import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -20,6 +24,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    progress(),
     viteCompression({
       verbose: true, // 默认即可
       disable: false, // 开启压缩(不禁用)，默认即可
@@ -28,20 +33,25 @@ export default defineConfig({
       algorithm: 'gzip', // 压缩算法
       ext: '.gz' // 文件类型
     }),
-    // visualizer(),
-    // viteImagemin() src/assets/JING411.jpg
+    ViteRestart({
+      restart: ['*.config.[jt]s', '**/config/*.[jt]s', '*.config.cjs']
+    }),
+    legacy()
   ],
   css: {
     devSourcemap: true
   },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          echarts: ['echarts']
-        }
-      }
-    }
+    //打包文件名称
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   }
 /*   server: {
     host: '192.168.3.88',
